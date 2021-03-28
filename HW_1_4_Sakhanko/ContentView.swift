db94b99a6bce6ea6a0c55a0b000207ef4fe31ce8
Pavel Sakhanko <iospaveldev@gmail.com>
2021-03-28 21:43:00 +0200

    Initial Commit

---
+++ HW_1_4_Sakhanko/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW_1_4_Sakhanko
+//
+//  Created by Pavel Sakhanko on 28.03.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HW_1_4_Sakhanko/HW_1_4_SakhankoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW_1_4_SakhankoApp.swift
+//  HW_1_4_Sakhanko
+//
+//  Created by Pavel Sakhanko on 28.03.21.
+//
+
+import SwiftUI
+
+@main
+struct HW_1_4_SakhankoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
